9f66231f9d16b6763e074e2a21da3a6b62920e6b
MarcoCocco <marcococco28@gmail.com>
2023-02-28 18:46:25 +0100

    inserito pseudo codice del carosello

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
+//1- definisci un array di immagini.
+//2- ottieni un riferimento all'elemento HTML che mostra l'immagine 
+//   principale del carosello.
+//3- ottieni i riferimenti agli elementi HTML che rappresentano 
+//   le frecce del carosello.
+//4- inizializza l'indice corrente dell'immagine.
+//5- mostra l'immagine iniziale sul carosello.
+//6- aggiungi un gestore di eventi alla freccia giù.
+//7- incrementa l'indice corrente dell'immagine ad ogni click.
+//8- mostra l'immagine corrente sul carosello.
+//9- aggiungi un gestore di eventi alla freccia su.
+//10- decrementa l'indice corrente dell'immagine ad ogni click.
+//11- mostra l'immagine corrente sul carosello.
 
-
-
-
-const carouselImages = [
+// definisci un array di immagini
+let carouselImages = [
     'img/01.webp',
     'img/02.webp',
     'img/03.webp',
     'img/04.webp',
     'img/05.webp'
 ];
-
-console.log(carouselImages);
 
 
 
